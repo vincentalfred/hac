043c36b0a902f5723ff2e77a6a8cdc8149084f1d
vincent <vincentalfred1@gmail.com>
2019-02-15 14:09:57 +0700

    Added profile and card model.

--- hac/models.py
+++ hac/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile (models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	nim = models.CharField(max_length=10, db_index=True, unique=True)
+	name = models.CharField(max_length=200)	#set db_index=True
+
+class Card (models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+	card_uid = models.CharField(max_length=8, db_index=True, unique=True)
+	
+
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'hac.apps.HacConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
