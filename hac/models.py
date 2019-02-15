72b381a32114076240b6fba95e20b6588e814f3a
vincent <vincentalfred1@gmail.com>
2019-02-15 15:19:59 +0700

    add models:certification, machine_type, machine, tap, usage.

--- hac/models.py
+++ hac/models.py
@@ -9,6 +9,27 @@ class Profile (models.Model):
 class Card (models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 	card_uid = models.CharField(max_length=8, db_index=True, unique=True)
-	
 
+class Machine_type (models.Model):
+	machine_type_name = models.CharField(max_length=200, db_index=True, unique=True)
 
+class Certification (models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	machine_type = models.ForeignKey(Machine_type, on_delete=models.CASCADE)
+
+class Machine (models.Model):
+	machine_type = models.ForeignKey(Machine_type,on_delete=models.CASCADE)
+	machine_name = models.CharField(max_length=200)
+
+class Tap (models.Model):
+	card = models.ForeignKey(Card, on_delete=models.DO_NOTHING)
+	machine = models.ForeignKey(Machine, on_delete=models.DO_NOTHING)
+	tap_time = models.DateTimeField()
+	power_usage = models.IntegerField(null=True, blank=True)
+
+class Usage (models.Model):
+	card = models.ForeignKey(Card, on_delete=models.DO_NOTHING)
+	machine = models.ForeignKey(Machine, on_delete=models.DO_NOTHING)
+	start_time = models.DateTimeField()
+	end_time = models.DateTimeField()
+	total_usage = models.IntegerField()
