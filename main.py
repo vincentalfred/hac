cefa852b96180776a8d2ecae915ea0e3956250e7
vincent <vincentalfred1@gmail.com>
2019-02-21 17:29:09 +0700

    edit main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from mysite.wsgi import application
-# App Engine by default looks for a main.py file at the root of the app
+# from mysite.wsgi import application
+from hac.wsgi import application
+
+# App Engine by default looks foxr a main.py file at the root of the app
 # directory with a WSGI-compatible object called app.
 # This file imports the WSGI-compatible object of the Django app,
 # application from mysite/wsgi.py and renames it app so it is
