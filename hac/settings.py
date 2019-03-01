0976b5cc3a4b6a37533a4e707b415ecdc2fba4f5
vincent <vincentalfred1@gmail.com>
2019-03-01 16:48:48 +0700

    edit gitignore

--- hac/settings.py
+++ hac/settings.py
@@ -114,10 +114,16 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.mysql',
             'HOST': '127.0.0.1',
-            'PORT': '3306',
+            # 'PORT': '3306',
             'NAME': 'hac',
             'USER': 'root',
             'PASSWORD': 'root',
+            # 'ENGINE': 'django.db.backends.mysql',
+            # 'HOST': '127.0.0.1',
+            # 'PORT': '3306',
+            # 'NAME': 'hac',
+            # 'USER': 'root',
+            # 'PASSWORD': 'root',
         }
     }
 
