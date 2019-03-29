d204f37dd9f4a6ff8719ba246cd74d0acc914cc4
vincent <vincentalfred1@gmail.com>
2019-03-29 15:50:57 +0700

    filter button

--- apps/usages/views.py
+++ apps/usages/views.py
@@ -4,9 +4,15 @@ from django.urls import reverse
 from django.views import generic
 
 from . models import Usage
+from apps.machines.models import Machine_type
 
 class IndexView(generic.ListView):
 	context_object_name = 'latest_usage_list'
-
+	
+	def get_context_data(self, **kwargs):
+		context = super(IndexView, self).get_context_data(**kwargs)
+		context['machine_type_list'] = Machine_type.objects.all()
+		return context
+		
 	def get_queryset(self):
 		return Usage.objects.order_by('-start_time')
