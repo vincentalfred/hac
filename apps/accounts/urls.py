dd70867c488a9be1437dc5904f446cd3be0ca9bc
WjyChandra <chandrawijaya53@yahoo.co.id>
2019-04-12 15:53:17 +0700

    Fixing Login Page & Adding Logout Page

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,10 +1,14 @@
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 from . import views
 
 
 app_name = 'accounts'
 urlpatterns = [
 	path('', views.IndexView.as_view(), name='index'),
-	path('login/', views.LoginView.as_view(), name='login'), 
-	path('<int:pk>/',views.DetailView.as_view(), name='detail'), 
+	# path('login/', views.LoginView.as_view(), name='login'), 
+	path('login/', auth_views.LoginView.as_view(template_name='accounts/accounts_login.html'), name='login'),
+	path('logout/', auth_views.LogoutView.as_view(template_name='accounts/accounts_logout.html'), name='logout'),
+	#path('logout/', auth_views.LogoutView.as_view(template_name='accounts/accounts_logout.html'), {'next_page': '/'}, name='logout'),
+	path('<int:pk>/', views.DetailView.as_view(), name='detail'), 
 ]
