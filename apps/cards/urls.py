95e01bb03cafca81d203747e3dc6a52ead0e4c1e
vincent <vincentalfred1@gmail.com>
2019-05-09 17:19:22 +0700

    register card to user

--- apps/cards/urls.py
+++ apps/cards/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'cards'
 urlpatterns = [
 	path('', views.IndexView.as_view(), name='index'),
+	path('<int:pk>/add/', views.CardCreate, name='card_add'),
 ]
--- apps/cards/views.py
+++ apps/cards/views.py
@@ -3,16 +3,13 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 
 from . models import Card, Unregistered_card
+from django.contrib.auth.models import User
 from apps.machines.models import Machine_type
-
-# class IndexView(generic.ListView):
-# 	template_name = 'usages/index.html'
-# 	context_object_name = ''
-
-# 	def get_queryset(self):
-# 		return Usages
+from apps.accounts.models import Profile
 
 class IndexView(LoginRequiredMixin, generic.ListView):
 	template_name = 'cards/cards_list.html'
@@ -25,7 +22,21 @@ class IndexView(LoginRequiredMixin, generic.ListView):
 		context['unregistered_cards_list'] = Unregistered_card.objects.all()
 		return context
 
-
-
-
-	
+@login_required
+def CardCreate(request, pk):
+	unregistered_card = Unregistered_card.objects.get(pk=pk)
+	if request.method == 'POST':
+		carduid = unregistered_card.card_uid
+		user_id = request.POST.get("selected_user")
+		user = User.objects.get(id=user_id)
+		unregistered_card.delete()
+		# add to card
+		card = Card(user = user, card_uid = carduid)
+		card.save()
+		return redirect('cards:index')
+	else:
+		users_list = User.objects.all().order_by('username')
+	return render(request, 'cards/cards_signup.html', {
+		'unregistered_card': unregistered_card,
+		'users_list': users_list,
+	})
