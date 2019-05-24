2a2f38f852c9d92fc7aeba77f18f6aa92aa646ea
vincent <vincentalfred1@gmail.com>
2019-05-24 14:39:55 +0700

    merge to master

--- apps/certifications/urls.py
+++ apps/certifications/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
 	path('<int:pk>/', views.CertificationDetail.as_view(), name='certification_detail'), 
 	path('<int:pk>/edit/', views.CertificationUpdate.as_view(), name='certification_update'),
 	path('<int:pk>/delete/', views.CertificationDelete.as_view(), name='certification_delete'),
+
+	path('users/<int:user_id>/', views.CertificationUserList.as_view(), name='certification_user_list'),
 ]
--- apps/certifications/views.py
+++ apps/certifications/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect, HttpResponse
+
 from . models import Certification
+from apps.machines.models import Machine, Machine_type
 
 class IndexView(LoginRequiredMixin, generic.ListView):
 	context_object_name = 'certification_list'
@@ -28,3 +31,20 @@ class CertificationDelete(LoginRequiredMixin, generic.DeleteView):
 	model = Certification
 	def get_success_url(self):
 		return reverse('certifications:index')
+
+class CertificationUserList(LoginRequiredMixin, generic.ListView):
+	template_name = 'certifications/certification_user_list.html'
+	context_object_name = 'machine_types'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['certification_list'] = {}
+		machine_types = Machine_type.objects.all()
+		for machine_type in machine_types:
+			if Certification.objects.filter(user = self.kwargs['user_id'] ,machine_type = machine_type).exists():
+				context['certification_list'][machine_type.machine_type_name] = True
+			else:
+				context['certification_list'][machine_type.machine_type_name] = False
+		return context
+	def get_queryset(self, *args, **kwargs):
+		return Machine_type.objects.all()
