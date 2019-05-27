dc9f649a9484fd1a21882789ec170b35659f1791
vincent <vincentalfred1@gmail.com>
2019-05-27 22:51:22 +0700

    usage page for specific user, machine and machine_type

--- apps/usages/views.py
+++ apps/usages/views.py
@@ -1,17 +1,17 @@
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
-from django.views import generic
+from django.views import generic, View
 from django_tables2 import RequestConfig
 from django.utils import timezone
-
-
+from django.core.serializers import serialize
 
 from .models import Usage, DailyUsage
 from .tables import UsageTable
-from apps.machines.models import Machine_type
-from .forms import MyForm
+from apps.machines.models import Machine, Machine_type
+from apps.accounts.models import Profile
+from .forms import DatePickerForm
 
 class IndexView(LoginRequiredMixin, generic.ListView):
 	context_object_name = 'latest_usage_list'
@@ -24,17 +24,14 @@ class IndexView(LoginRequiredMixin, generic.ListView):
 		return context
 		
 	def get_queryset(self):
-		# return Usage.objects.all()
-		# return Usage.objects.order_by('-start_time')
 		return Usage.objects.order_by('-start_time')[:20]
-		# return Usage.objects.latest('start_time')
 
 def DetailView(request):
 	if request.user.is_authenticated: 
 		if request.method == 'POST': 
-			form = MyForm(request.POST)
+			form = DatePickerForm(request.POST)
 			if form.is_valid():
-				start_time = form.cleaned_data['start_date_field']
+				start_time = form.cleaned_data['start_date_field'].replace(hour=0, minute=0, second=0, microsecond=0)
 				end_time = form.cleaned_data['end_date_field']
 				end_time = end_time+timezone.timedelta(hours = 23, minutes = 59)
 				dailyUsage_list = DailyUsage.objects.filter(date__range = [start_time , end_time])
@@ -43,8 +40,8 @@ def DetailView(request):
 				machine_type_list = Machine_type.objects.all()
 				
 		else:
-			form = MyForm()
-			start_time = (timezone.now() + timezone.timedelta(days=-7))
+			form = DatePickerForm()
+			start_time = (timezone.now() + timezone.timedelta(days=-7)).replace(hour=0, minute=0, second=0, microsecond=0)
 			end_time = timezone.now()
 			usage_table = UsageTable(Usage.objects.filter(start_time__range = [start_time , end_time]))
 			RequestConfig(request, paginate={'per_page': 10}).configure(usage_table)
@@ -60,16 +57,16 @@ def DetailView(request):
 
 	else: 
 		if request.method == 'POST': 
-			form = MyForm(request.POST)
+			form = DatePickerForm(request.POST)
 			if form.is_valid():
-				start_time = form.cleaned_data['start_date_field']
+				start_time = form.cleaned_data['start_date_field'].replace(hour=0, minute=0, second=0, microsecond=0)
 				end_time = form.cleaned_data['end_date_field']
 				end_time = end_time+timezone.timedelta(hours = 23, minutes = 59)
-				dailyUsage_list = DailyUsage.objects.filter(date__range = [start_time , end_time])
+				dailyUsage_list = DailyUsage.objects.filter(date__range = [start_time , end_time]).order_by()
 				machine_type_list = Machine_type.objects.all()
 		else:
-			form = MyForm()
-			start_time = (timezone.now() + timezone.timedelta(days=-7))
+			form = DatePickerForm()
+			start_time = (timezone.now() + timezone.timedelta(days=-7)).replace(hour=0, minute=0, second=0, microsecond=0)
 			end_time = timezone.now()
 			machine_type_list = Machine_type.objects.all()
 			dailyUsage_list = DailyUsage.objects.filter(date__range = [start_time , end_time])
@@ -80,4 +77,217 @@ def DetailView(request):
 			'form': form,
 		})
 
+class UserView(LoginRequiredMixin, View):
+	def get(self, request, *args, **kwargs):
+		profile = Profile.objects.get(id=self.kwargs["user_id"])
+		form = DatePickerForm()
+		start_time = (timezone.now() + timezone.timedelta(days=-7)).replace(hour=0, minute=0, second=0, microsecond=0)
+		end_time = timezone.now()
 
+		timeData = {}
+		energyData = {}
+		machine_types = Machine_type.objects.all()
+		for machine_type in machine_types:
+			timeData[machine_type.machine_type_name] = {}
+			energyData[machine_type.machine_type_name] = {}
+		dateList = []
+		cur_date = start_time
+		while cur_date < end_time:
+			dateList.append(cur_date)
+			for machine_type in machine_types:
+				timeData[machine_type.machine_type_name][cur_date.strftime("%d/%m/%y")] = 0
+				energyData[machine_type.machine_type_name][cur_date.strftime("%d/%m/%y")] = 0
+			cur_date += timezone.timedelta(days=1)
+		dateList.append(cur_date)
+
+		for machine_type in machine_types:
+			usages = Usage.objects.filter(user=profile.user, machine_type=machine_type, start_time__range=[start_time, end_time])
+			for usage in usages:
+				timeData[machine_type.machine_type_name][usage.start_time.strftime("%d/%m/%y")] += ((usage.end_time-usage.start_time).seconds/60)
+				energyData[machine_type.machine_type_name][usage.start_time.strftime("%d/%m/%y")] += usage.total_usage
+
+		usage_table = UsageTable(Usage.objects.filter(user=profile.user, start_time__range=[start_time , end_time]))
+		return render(request, 'usages/usage_user_list.html', {
+			'usage_table': usage_table,
+			'machine_type_list': machine_types,
+			'form': form,
+			'profile': profile,
+			'dateList': dateList,
+			'timeData': timeData,
+			'energyData': energyData,
+		})
+
+	def post(self, request, *args, **kwargs):
+		form = DatePickerForm(request.POST)
+		if form.is_valid():
+			profile = Profile.objects.get(id=self.kwargs["user_id"])
+			start_time = form.cleaned_data['start_date_field'].replace(hour=0, minute=0, second=0, microsecond=0)
+			end_time = form.cleaned_data['end_date_field']
+			end_time = end_time+timezone.timedelta(hours = 23, minutes = 59)
+
+			timeData = {}
+			energyData = {}
+			machine_types = Machine_type.objects.all()
+			for machine_type in machine_types:
+				timeData[machine_type.machine_type_name] = {}
+				energyData[machine_type.machine_type_name] = {}
+			dateList = []
+			cur_date = start_time
+			while cur_date < end_time:
+				dateList.append(cur_date)
+				for machine_type in machine_types:
+					timeData[machine_type.machine_type_name][cur_date.strftime("%d/%m/%y")] = 0
+					energyData[machine_type.machine_type_name][cur_date.strftime("%d/%m/%y")] = 0
+				cur_date += timezone.timedelta(days=1)
+			dateList.append(cur_date)
+
+			for machine_type in machine_types:
+				usages = Usage.objects.filter(user=profile.user, machine_type=machine_type, start_time__range=[start_time, end_time])
+				for usage in usages:
+					timeData[machine_type.machine_type_name][usage.start_time.strftime("%d/%m/%y")] += ((usage.end_time-usage.start_time).seconds/60)
+					energyData[machine_type.machine_type_name][usage.start_time.strftime("%d/%m/%y")] += usage.total_usage
+
+			usage_table = UsageTable(Usage.objects.filter(user=profile.user, start_time__range = [start_time , end_time]))
+			return render(request, 'usages/usage_user_list.html', {
+				'usage_table': usage_table,
+				'machine_type_list': machine_types,
+				'form': form,
+				'profile': profile,
+				'dateList': dateList,
+				'timeData': timeData,
+				'energyData': energyData,
+			})
+
+class MachineView(LoginRequiredMixin, View):
+	def get(self, request, *args, **kwargs):
+		machine = Machine.objects.get(id=self.kwargs["machine_id"])
+		form = DatePickerForm()
+		start_time = (timezone.now() + timezone.timedelta(days=-7)).replace(hour=0, minute=0, second=0, microsecond=0)
+		end_time = timezone.now()
+
+		dateList = []
+		timeData = {}
+		energyData = {}
+		cur_date = start_time
+		while cur_date < end_time:
+			dateList.append(cur_date)
+			timeData[cur_date.strftime("%d/%m/%y")] = 0
+			energyData[cur_date.strftime("%d/%m/%y")] = 0
+			cur_date += timezone.timedelta(days=1)
+		dateList.append(cur_date)
+
+		usages = Usage.objects.filter(machine=machine, start_time__range=[start_time, end_time])
+		for usage in usages:
+			timeData[usage.start_time.strftime("%d/%m/%y")] += ((usage.end_time-usage.start_time).seconds/60)
+			energyData[usage.start_time.strftime("%d/%m/%y")] += usage.total_usage
+
+		usage_table = UsageTable(Usage.objects.filter(machine=machine, start_time__range=[start_time , end_time]))
+		return render(request, 'usages/usage_machine_list.html', {
+			'usage_table': usage_table,
+			'form': form,
+			'machine': machine,
+			'dateList': dateList,
+			'timeData': timeData,
+			'energyData': energyData,
+		})
+
+	def post(self, request, *args, **kwargs):
+		form = DatePickerForm(request.POST)
+		if form.is_valid():
+			machine = Machine.objects.get(id=self.kwargs["machine_id"])
+			start_time = form.cleaned_data['start_date_field'].replace(hour=0, minute=0, second=0, microsecond=0)
+			end_time = form.cleaned_data['end_date_field']
+			end_time = end_time+timezone.timedelta(hours = 23, minutes = 59)
+
+			dateList = []
+			timeData = {}
+			energyData = {}
+			cur_date = start_time
+			while cur_date < end_time:
+				dateList.append(cur_date)
+				timeData[cur_date.strftime("%d/%m/%y")] = 0
+				energyData[cur_date.strftime("%d/%m/%y")] = 0
+				cur_date += timezone.timedelta(days=1)
+			dateList.append(cur_date)
+
+			usages = Usage.objects.filter(machine=machine, start_time__range=[start_time, end_time])
+			for usage in usages:
+				timeData[usage.start_time.strftime("%d/%m/%y")] += ((usage.end_time-usage.start_time).seconds/60)
+				energyData[usage.start_time.strftime("%d/%m/%y")] += usage.total_usage
+			
+			usage_table = UsageTable(Usage.objects.filter(machine=machine, start_time__range=[start_time , end_time]))
+			return render(request, 'usages/usage_machine_list.html', {
+				'usage_table': usage_table,
+				'form': form,
+				'machine': machine,
+				'dateList': dateList,
+				'timeData': timeData,
+				'energyData': energyData,
+			})
+
+class MachineTypeView(LoginRequiredMixin, View):
+	def get(self, request, *args, **kwargs):
+		machine_type = Machine_type.objects.get(id=self.kwargs["machine_type_id"])
+		form = DatePickerForm()
+		start_time = (timezone.now() + timezone.timedelta(days=-7)).replace(hour=0, minute=0, second=0, microsecond=0)
+		end_time = timezone.now()
+
+		dateList = []
+		timeData = {}
+		energyData = {}
+		cur_date = start_time
+		while cur_date < end_time:
+			dateList.append(cur_date)
+			timeData[cur_date.strftime("%d/%m/%y")] = 0
+			energyData[cur_date.strftime("%d/%m/%y")] = 0
+			cur_date += timezone.timedelta(days=1)
+		dateList.append(cur_date)
+
+		usages = Usage.objects.filter(machine_type=machine_type, start_time__range=[start_time, end_time])
+		for usage in usages:
+			timeData[usage.start_time.strftime("%d/%m/%y")] += ((usage.end_time-usage.start_time).seconds/60)
+			energyData[usage.start_time.strftime("%d/%m/%y")] += usage.total_usage
+
+		usage_table = UsageTable(Usage.objects.filter(machine_type=machine_type, start_time__range=[start_time , end_time]))
+		return render(request, 'usages/usage_machine_type_list.html', {
+			'usage_table': usage_table,
+			'form': form,
+			'machine_type': machine_type,
+			'dateList': dateList,
+			'timeData': timeData,
+			'energyData': energyData,
+		})
+
+	def post(self, request, *args, **kwargs):
+		form = DatePickerForm(request.POST)
+		if form.is_valid():
+			machine_type = Machine_type.objects.get(id=self.kwargs["machine_type_id"])
+			start_time = form.cleaned_data['start_date_field'].replace(hour=0, minute=0, second=0, microsecond=0)
+			end_time = form.cleaned_data['end_date_field']
+			end_time = end_time+timezone.timedelta(hours = 23, minutes = 59)
+
+			dateList = []
+			timeData = {}
+			energyData = {}
+			cur_date = start_time
+			while cur_date < end_time:
+				dateList.append(cur_date)
+				timeData[cur_date.strftime("%d/%m/%y")] = 0
+				energyData[cur_date.strftime("%d/%m/%y")] = 0
+				cur_date += timezone.timedelta(days=1)
+			dateList.append(cur_date)
+
+			usages = Usage.objects.filter(machine_type=machine_type, start_time__range=[start_time, end_time])
+			for usage in usages:
+				timeData[usage.start_time.strftime("%d/%m/%y")] += ((usage.end_time-usage.start_time).seconds/60)
+				energyData[usage.start_time.strftime("%d/%m/%y")] += usage.total_usage
+			
+			usage_table = UsageTable(Usage.objects.filter(machine_type = self.kwargs["machine_type_id"], start_time__range = [start_time , end_time]))
+			return render(request, 'usages/usage_machine_type_list.html', {
+				'usage_table': usage_table,
+				'form': form,
+				'machine_type': machine_type,
+				'dateList': dateList,
+				'timeData': timeData,
+				'energyData': energyData,
+			})
