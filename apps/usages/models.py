bc61ffd63e17704325e12f7a535d8386bc7570cb
vincent <vincentalfred1@gmail.com>
2019-08-12 16:40:49 +0700

    total usage (Wh)

--- apps/usages/models.py
+++ apps/usages/models.py
@@ -9,10 +9,10 @@ class Usage (models.Model):
 	machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
 	start_time = models.DateTimeField()
 	end_time = models.DateTimeField()
-	total_usage = models.IntegerField()
+	total_usage = models.IntegerField(verbose_name='Total Usage (Wh)')
 
 class DailyUsage (models.Model):
 	date = models.DateField(db_index=True)
 	machine_type = models.ForeignKey(Machine_type, on_delete=models.CASCADE)
-	total_usage = models.IntegerField(default=0)
+	total_usage = models.IntegerField(default=0, verbose_name='Total Usage (Wh)')
 	total_time = models.BigIntegerField(default=0)
